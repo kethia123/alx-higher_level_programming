72d7b6d19372cba4037a257eb55a0304a2095f8c
kethia123 <kemykayigire@gmail.com>
2023-03-15 11:29:47 +0200

    New changes

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,7 +6,7 @@ Object.keys(dict).map(function (key, index) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
   }
-  newDict[dict[key]].push(key);
+  return newDict[dict[key]].push(key);
 });
 
 console.log(newDict);
